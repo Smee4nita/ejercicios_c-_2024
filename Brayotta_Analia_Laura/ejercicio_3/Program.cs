1bac9c3f621fa10be32ed5f7ea51d26649ce1b19
Smee4nita <b.analia.laura@gmail.com>
2024-03-23 17:17:07 -0300

    ejercicio 3

---
+++ Brayotta_Analia_Laura/ejercicio_3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ejercicio_3
+{ 
+    //Ingresar 2 números y mostrar la suma de los mismos.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string numUnoTexto;
+            string numDosTexto;
+            int numeroUno;
+            int numeroDos;
+            int resultado;
+            
+            Console.WriteLine("Hola, ingrese un número, por favor");
+            numUnoTexto = Console.ReadLine();
+            numeroUno = int.Parse(numUnoTexto);
+            Console.WriteLine("Por favor, ingrese otro número");
+            numDosTexto =Console.ReadLine();
+            numeroDos = int.Parse(numDosTexto);
+            resultado = numeroUno + numeroDos;
+            Console.WriteLine($"EL resultado es = {resultado}");
+
+        }
+    }
+}
